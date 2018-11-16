ec7186e04848b1d83eddfa341564941ad205ffec
kneepdee <gabriel.kuslap@gmail.com>
2018-11-16 21:05:11 +0200

    Add brush size adjustment, clear canvas button

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 const canvasWrapper = document.querySelector('.canvas-wrapper');
-const maxSizeSlider = document.querySelector('#max-size');
-const minSizeSlider = document.querySelector('#min-size');
-const clearCanvas = document.querySelector('.clear-canvas');
+const maxSizeSlider = document.querySelector('#maxSize');
+const minSizeSlider = document.querySelector('#minSize');
+const clearCanvas = document.querySelector('.clear-canvas-btn');
+const maxSizeOutput = document.querySelector('#maxSizeValueDisplayId');
 
 // console.log('canvasWrapper width: ' + canvasWrapper.offsetWidth);
 // console.log('canvasWrapper height: ' + canvasWrapper.offsetHeight);
@@ -25,65 +26,66 @@ let hue = 0;
 let direction = true;
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 function draw(e) {
-    if (!isDrawing) return // stops the fn from running when they are not mouse
-    // console.log(e.touches[0].clientX);
-    hue = getRandomInt(360);
-    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-    hue > 360 ? hue = 0 : hue;
+  if (!isDrawing) return; // stops the fn from running when they are not mouse
+  // console.log(e.touches[0].clientX);
+  hue = getRandomInt(360);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  // hue > 360 ? hue = 0 : hue;
+  // console.log(hue);
 
-    if ((+minSizeSlider.value > +maxSizeSlider.value) || (+minSizeSlider.value === +maxSizeSlider.value)) {
-        ctx.lineWidth = +maxSizeSlider.value;
-        maxSizeSlider.value = minSizeSlider.value;
-    } else {
-        if (ctx.lineWidth === +maxSizeSlider.value || ctx.lineWidth === +minSizeSlider.value) {
-            direction = !direction;
-        }
-        console.log(direction);
-
-        direction === true ? ctx.lineWidth++ : ctx.lineWidth--;
+  if ((+minSizeSlider.value > +maxSizeSlider.value) || (+minSizeSlider.value === +maxSizeSlider.value)) {
+    ctx.lineWidth = +maxSizeSlider.value;
+    maxSizeSlider.value = minSizeSlider.value;
+    maxSizeOutput.innerHTML = maxSizeSlider.value;
+  } else {
+    if (ctx.lineWidth === +maxSizeSlider.value || ctx.lineWidth === +minSizeSlider.value) {
+      direction = !direction;
     }
-    console.log(ctx.lineWidth);
-    // if (ctx.lineWidth === +maxSizeSlider.value || ctx.lineWidth === +minSizeSlider.value) {
-    //     direction = !direction;
-    // }
-    // console.log(direction);
+    console.log(direction);
+  }
+  console.log(ctx.lineWidth);
+  direction === true ? ctx.lineWidth++ : ctx.lineWidth--;
+  // if (ctx.lineWidth === +maxSizeSlider.value || ctx.lineWidth === +minSizeSlider.value) {
+  //     direction = !direction;
+  // }
+  // console.log(direction);
 
-    // direction === true ? ctx.lineWidth++ : ctx.lineWidth--;
+  // direction === true ? ctx.lineWidth++ : ctx.lineWidth--;
 
-    ctx.beginPath();
-    // start from
-    ctx.moveTo(lastX, lastY);
-    // go to
+  ctx.beginPath();
+  // start from
+  ctx.moveTo(lastX, lastY);
+  // go to
 
-    if ('touches' in e) {
-        ctx.lineTo(e.touches[0].clientX, e.touches[0].clientY);
-    } else {
-        ctx.lineTo(e.offsetX, e.offsetY);
-    };
+  if ('touches' in e) {
+    ctx.lineTo(e.touches[0].clientX, e.touches[0].clientY);
+  } else {
+    ctx.lineTo(e.offsetX, e.offsetY);
+  }
 
-    ctx.stroke();
+  ctx.stroke();
 
-    if ('touches' in e) {
-        [lastX, lastY] = [e.touches[0].clientX, e.touches[0].clientY];
-    } else {
-        [lastX, lastY] = [e.offsetX, e.offsetY];
-    };
+  if ('touches' in e) {
+    [lastX, lastY] = [e.touches[0].clientX, e.touches[0].clientY];
+  } else {
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+  }
 }
 
 // touch events
 
 canvas.addEventListener('touchstart', (e) => {
-    isDrawing = true;
-    ctx.lineWidth = +minSizeSlider.value + 1;
-    if ('touches' in e) {
-        [lastX, lastY] = [e.touches[0].clientX, e.touches[0].clientY];
-    } else {
-        [lastX, lastY] = [e.offsetX, e.offsetY];
-    };
+  isDrawing = true;
+  ctx.lineWidth = +minSizeSlider.value + 1;
+  if ('touches' in e) {
+    [lastX, lastY] = [e.touches[0].clientX, e.touches[0].clientY];
+  } else {
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+  }
 });
 
 canvas.addEventListener('touchmove', draw);
@@ -93,10 +95,10 @@ canvas.addEventListener('mouseout', () => isDrawing = false);
 // mouse events
 
 canvas.addEventListener('mousedown', (e) => {
-    isDrawing = true;
-    console.log('mousedown');
-    ctx.lineWidth = +minSizeSlider.value + 1;
-    [lastX, lastY] = [e.offsetX, e.offsetY]; // destructuring
+  isDrawing = true;
+  console.log('mousedown');
+  ctx.lineWidth = +minSizeSlider.value + 1;
+  [lastX, lastY] = [e.offsetX, e.offsetY]; // destructuring
 });
 
 canvas.addEventListener('mousemove', draw);
@@ -104,28 +106,29 @@ canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
 // reset canvas
-clearCanvas.addEventListener('click', () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // minSizeSlider.value = +minSizeSlider.value + 30;
-})
+
+const resetCanvas = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+clearCanvas.addEventListener('click', resetCanvas);
 
 // Prevent scrolling when touching the canvas
-document.body.addEventListener("touchstart", function (e) {
-    if (e.target == canvas) {
-        e.preventDefault();
-    }
+document.body.addEventListener('touchstart', (e) => {
+  if (e.target === canvas) {
+    e.preventDefault();
+  }
 }, false);
-document.body.addEventListener("touchend", function (e) {
-    if (e.target == canvas) {
-        e.preventDefault();
-    }
+document.body.addEventListener('touchend', (e) => {
+  if (e.target === canvas) {
+    e.preventDefault();
+  }
 }, false);
-document.body.addEventListener("touchmove", function (e) {
-    if (e.target == canvas) {
-        e.preventDefault();
-    }
+document.body.addEventListener('touchmove', (e) => {
+  if (e.target === canvas) {
+    e.preventDefault();
+  }
 }, false);
-
 
 
 // positioning div element;
