4e5bf1b567b913a555b116a9d5277b5eeea0a702
kneepdee <gabriel.kuslap@gmail.com>
2018-11-18 11:12:09 +0200

    clean up code

--- scripts.js
+++ scripts.js
@@ -6,18 +6,10 @@ const minSizeSlider = document.querySelector('#minSize');
 const clearCanvas = document.querySelector('.clear-canvas-btn');
 const maxSizeOutput = document.querySelector('#maxSizeValueDisplayId');
 
-// console.log('canvasWrapper width: ' + canvasWrapper.offsetWidth);
-// console.log('canvasWrapper height: ' + canvasWrapper.offsetHeight);
-
-// console.log('knobs width: ' + knobs.clientWidth);
-// console.log('knobs height: ' + knobs.clientHeight);
-
 canvas.width = canvasWrapper.offsetWidth;
 canvas.height = canvasWrapper.offsetHeight;
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-// ctx.lineWidth = +minSizeSlider.value + 1;
-// ctx.globalCompositeOperation = 'luminosity';
 
 let isDrawing = false;
 let lastX = 0;
@@ -31,7 +23,6 @@ function getRandomInt(max) {
 
 function draw(e) {
   if (!isDrawing) return; // stops the fn from running when they are not mouse
-  // console.log(e.touches[0].clientX);
   hue = getRandomInt(360);
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
@@ -47,41 +38,17 @@ function draw(e) {
   } else {
     [lastX, lastY] = [e.offsetX, e.offsetY];
   }
-  // hue > 360 ? hue = 0 : hue;
-  // console.log(hue);
-
-  // if (+minSizeSlider.value === +maxSizeSlider.value) {
-  //   ctx.lineWidth = +minSizeSlider.value;
-  // }
   if (ctx.lineWidth >= +maxSizeSlider.value || ctx.lineWidth <= +minSizeSlider.value) {
     direction = !direction;
   }
-
   if (direction) {
     ctx.lineWidth++;
   } else {
     ctx.lineWidth--;
   }
-
-  console.log(direction);
-  console.log(ctx.lineWidth);
-  direction === true ? ctx.lineWidth++ : ctx.lineWidth--;
-  // if (ctx.lineWidth === +maxSizeSlider.value || ctx.lineWidth === ++minSizeSlider.valueSizeSlider.value) {
-  //     direction = !direction;
-  // }
-  // console.log(direction);
-
-  // direction === true ? ctx.lineWidth++ : ctx.lineWidth--;
-
-  // start from
-  // go to
-
-
-
 }
 
 // touch events
-
 canvas.addEventListener('touchstart', (e) => {
   isDrawing = true;
   if (+maxSizeSlider.value < +minSizeSlider.value) {
@@ -101,7 +68,6 @@ canvas.addEventListener('touchend', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
 // mouse events
-
 canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
   console.log('mousedown');
@@ -118,7 +84,6 @@ canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
 // reset canvas
-
 const resetCanvas = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
@@ -141,22 +106,3 @@ document.body.addEventListener('touchmove', (e) => {
     e.preventDefault();
   }
 }, false);
-
-
-// positioning div element;
-
-// const body = document.querySelector('body');
-// const bodyHeight = body.clientHeight;
-
-// const textDiv = document.querySelector('.title');
-// const textDivHeight = textDiv.clientHeight;
-
-// console.log(bodyHeight);
-
-// console.log(textDivHeight);
-
-// const margin = (bodyHeight - textDivHeight * 7) / 2;
-
-// console.log(margin);
-
-// textDiv.style.marginTop = `${margin}px`;
